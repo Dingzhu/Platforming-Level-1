62bcb74991b591e6aaf9f531d75e6f18dc25eed6
Dingzhu <dingzhu3@gmail.com>
2017-11-29 15:12:12 -0800

    Game inserted

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,71 @@
+﻿/*Put on every Pickup prefab (ie every object that can be picked up)*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    public GameObject player;
+    public bool canClick;
+    public bool isHolding;
+    public Rigidbody2D rb;
+    public BoxCollider2D bc;
+
+    //Make a reference to the player
+    //canClick = whether the player can pick up the item or not
+    //isHolding = whether the player is currently holding the item or not
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        canClick = false;
+        isHolding = false;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //checking if the item isn't being carried by the player
+        if (gameObject.transform.parent != player.transform)
+        {
+            isHolding = false;
+            canClick = true;
+
+            if (gameObject.GetComponent<Rigidbody2D>() == null)
+            { 
+                gameObject.AddComponent<Rigidbody2D>();
+                rb = gameObject.GetComponent<Rigidbody2D>();
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
+            }
+        }
+    }
+
+    //Using a CircleCollider2D with a radius slightly bigger than the object itself, so the player can only pick it up if they are within the radius
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            canClick = true;
+        }
+    }
+
+    //What happens when the player clicks on the item
+    private void OnMouseDown()
+    {
+        //code only executes if the player isn't holding anything
+        if (canClick)
+        {
+            //Make the item a child of the player, currently the player is carrying it on their head lol
+            //also make it so it doesn't get jostled around when the player is carrying it
+            //by freezing the x position
+            gameObject.transform.parent = player.transform;
+            gameObject.transform.position = player.transform.position;
+            transform.localPosition = new Vector3(0, 0.5f, 0);
+            Destroy(rb);
+
+            //player can't pick up anymore items because they're holding one
+            canClick = false;
+            isHolding = true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Platforming-Test
+This is a Unity project that serves as a prototype for the platforming segments of my group's capstone game.
+
+### CONTROLS ###
+A & D to move left and right
+
+W to jump
+
+S to fall below a one-way platform
+
+Left-Click to pick up an item (must be in range of the item)
+
+E to drop item
+
+### CREDITS ###
+CharacterController2D.cs and DemoScene.cs || taken from https://github.com/prime31/CharacterController2D and modified
+
+angrychoco.png || taken from FINAL FANTASY XIV
+
+### STUFF I NEED TO DO ###
+- visual feedback on the pickups to show the player when they are able to pick it up
